1b33846c24a3653f7eeba62d4d10b089880200fc
Joseph <blazej.sabisz@cyfroweszkoly.pl>
2025-09-04 12:27:29 +0200

    first

---
+++ listaZadanBolkaILolka/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace listaZadanBolkaILolka;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
